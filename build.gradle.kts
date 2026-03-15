8c2ad8ff8dc098795916c418d8c691e45b7e8577
c4vxl <manage@c4vxl.de>
2026-03-15 10:41:16 +0100

    Added RankSystem

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("xyz.jpenilla.run-paper") version "3.0.2"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     `maven-publish`
+    `java-library`
 }
 
 group = "de.c4vxl"
@@ -13,6 +14,7 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://mvn.c4vxl.de/mc/")
 }
 
 dependencies {
@@ -24,6 +26,9 @@ dependencies {
     // Command API
     implementation("dev.jorel:commandapi-paper-shade:11.1.0")
     implementation("dev.jorel:commandapi-kotlin-paper:11.1.0")
+
+    // Rank System
+    api("de.c4vxl:RankSystem:1.0.0")
 }
 
 kotlin {
